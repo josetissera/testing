82dbdaf102b77239e83518e36e766ae6dd9ed884
Jose Tissera <josetiss18@gmail.com>
2023-07-12 02:08:42 -0300

    resolvimosTodosLosTest

--- src/agregarContacto.ts
+++ src/agregarContacto.ts
@@ -1,12 +1,18 @@
-import { Contacto, contactos } from "./contactos";
+import { Contacto } from "./contactos";
 
 function agregarContacto(lista:Contacto[], contactoNuevo:Contacto){
-    contactos.forEach(elem => {
-        if (elem === contactoNuevo){
-            return contactos
-        }
+    let estaRepetido: boolean = false    
+    let nuevaLista:Contacto[] = []
+    nuevaLista = nuevaLista.concat(lista)
+    lista.forEach(elem => {
+        if (elem.id === contactoNuevo.id){
+            estaRepetido = true
+       }
     });
-    return lista.push(contactoNuevo)
+    if (estaRepetido === false){
+        nuevaLista.push(contactoNuevo)
+    }
+    return nuevaLista
 }
 
 export {agregarContacto};
